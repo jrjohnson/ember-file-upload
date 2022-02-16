7f5803c6f79ee195c4a985916f0e69b351ce5a38
Jonathan Johnson <jon.johnson@ucsf.edu>
2022-02-15 20:53:01 -0800

    Remove custom build for mirage
    Just trying something, probably not worth it.

--- addon/mirage/index.js
+++ addon/mirage/index.js
@@ -7,18 +7,17 @@ import {
   importSync,
 } from '@embroider/macros';
 
-const NETWORK = {
-  wired: 50000, // 500 Mb/s
-  wifi: 15000, // 15 Mb/s
-  dsl: 1000, // 1 Mb/s
-  '4g': 3000, // 4 Mb/s
-  '3g': 250, // 250 kb/s
-  '2g': 50, // 50 kb/s
-  gprs: 20, // 20 kb/s
-  offline: 0,
-};
-
 export function upload(fn, options = { network: null, timeout: null }) {
+  const NETWORK = {
+    wired: 50000, // 500 Mb/s
+    wifi: 15000, // 15 Mb/s
+    dsl: 1000, // 1 Mb/s
+    '4g': 3000, // 4 Mb/s
+    '3g': 250, // 250 kb/s
+    '2g': 50, // 50 kb/s
+    gprs: 20, // 20 kb/s
+    offline: 0,
+  };
   if (
     macroCondition(
       dependencySatisfies('miragejs', '*') &&
